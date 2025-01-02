263919485e3923ea74b301e562d8077eeab102ba
samrat-rock <samratpandey715@gmail.com>
2025-01-02 11:23:24 +0545

    net

--- src/Components/Title/TitleCard.jsx
+++ src/Components/Title/TitleCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './TitleCard.css'
+import cards_data from '../../assets/cards/Cards_data'
 
 function TitleCard() {
   return (
     <div className='titlecard'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index)=>{
+                  return <div className='card' key={index}>
+                    <img src={card.image} alt="" /> 
+                    <p>{card.name}</p>
+                    </div>
+        })}
+      </div>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,10 @@ import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCard from '../../Components/Title/TitleCard'
+
 
 function Home() {
   return (
@@ -14,9 +18,11 @@ function Home() {
           <img src={hero_title} alt="" className='caption-img' />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, culpa rem? Libero necessitatibus dolorum, error nostrum officiis assumenda nulla iure atque eius incidunt temporibus fuga possimus illum voluptatum provident excepturi.
           </p>
-          <div className="hero-btn">
-           
+          <div className="hero-btns">
+           <button className='btn'><img src={play_icon} alt="" />play</button>
+           <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
           </div>
+          <TitleCard/>
         </div>
 
       </div>
